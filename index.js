526401e5ca4df0fa12f7b798bfe0263a1d431092
mtin79 <mtin79@gmail.com>
2017-08-15 17:56:06 +0200

    refactor: Improve game status messages and shorten code in functions.

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
 // Extend Array Class with method to shuffle cards.
 Array.prototype.shuffle = function() {
-    var input = this;
+  var input = this;
 
-    for (var i = input.length-1; i >=0; i--) {
+  for (var i = input.length - 1; i >= 0; i--) {
 
-        var randomIndex = Math.floor(Math.random()*(i+1));
-        var itemAtIndex = input[randomIndex];
+    var randomIndex = Math.floor(Math.random() * (i + 1));
+    var itemAtIndex = input[randomIndex];
 
-        input[randomIndex] = input[i];
-        input[i] = itemAtIndex;
-    }
-    return input;
+    input[randomIndex] = input[i];
+    input[i] = itemAtIndex;
+  }
+  return input;
 }
 
 // This object represents the memory game to be played.
+// It contains state and behavior of it.
 var game = {
   gameStarted: false,
   gameStartDate: undefined,
@@ -24,7 +25,7 @@ var game = {
   gameplayStarRating: 3,
   cardPairsFound: 0,
   drawStepCards: [undefined, undefined],
-  starRatingThresholds: [64,32,16],
+  starRatingThresholds: [64, 32, 16],
   availableStepsPerDraw: 1,
   gridOfCards: [
     "a", "a", "b", "b",
@@ -32,15 +33,15 @@ var game = {
     "e", "e", "f", "f",
     "g", "g", "h", "h"
   ],
-  startGame: function(){
+  startGame: function() {
     if (game.gameStarted === false) {
       game.gameStarted = true;
       game.gameStartDate = Date.now();
 
-      console.log("Game started: "+game.gameStarted+" ,"+game.gameStartDate);
+      console.log("Game started: " + game.gameStarted + " ," + game.gameStartDate);
     }
   },
-  endGame: function(){
+  endGame: function() {
     if (game.gameStarted === true) {
       game.gameStarted = false;
       game.gameEndDate = Date.now();
@@ -48,18 +49,18 @@ var game = {
     }
   },
   picturesForCards: {
-    a:"url('./media/board-game.jpg')",
-    b:"url('./media/cake.jpg')",
-    c:"url('./media/cardiff-castle.jpg')",
-    d:"url('./media/girls.jpg')",
-    e:"url('./media/globe.jpg')",
-    f:"url('./media/motorbike.jpg')",
-    g:"url('./media/pebbles.jpg')",
-    h:"url('./media/seagull.jpg')"
+    a: "url('./media/board-game.jpg')",
+    b: "url('./media/cake.jpg')",
+    c: "url('./media/cardiff-castle.jpg')",
+    d: "url('./media/girls.jpg')",
+    e: "url('./media/globe.jpg')",
+    f: "url('./media/motorbike.jpg')",
+    g: "url('./media/pebbles.jpg')",
+    h: "url('./media/seagull.jpg')"
   },
   defaultCardPicture: "url('./media/contemporary_china.png')",
 
-  initialize: function(){
+  initialize: function() {
     game.gameStarted = false;
     game.gameStartDate = undefined;
     game.gameEndDate = undefined;
@@ -70,31 +71,79 @@ var game = {
     game.cardPairsFound = 0;
     game.drawStepCards = [undefined, undefined];
     game.gridOfCards.shuffle();
+    $(".controls span").text("");
     $('[data-card-position]').removeClass("found");
     $('[data-card-position]').removeClass("memoryCard--mismatch");
     $('[data-card-position]').css("background-image", game.defaultCardPicture);
-    console.log("gridOfCards: "+this.gridOfCards);
+    console.log("gridOfCards: " + this.gridOfCards);
   },
-  updateGameStats: function(){
+  updateGameStats: function() {
     // update the moves counter showing the number of card pairs drawn.
     $(".controls__moves").text(parseInt(game.moves));
 
     // update the starRating according to the number of moves made.
-    $(".controls__rating").text(function(){
+    $(".controls__rating").text(function() {
       var starString = "";
       var starActiveString = '<i class="fa fa-star" aria-hidden="true"></i>';
       var starInactiveString = '<i class="fa fa-star-o" aria-hidden="true"></i>';
       if (game.moves > 0 && game.moves <= game.starRatingThresholds[2]) {
         starString = starActiveString + starActiveString + starActiveString;
-      } else if (game.moves <= game.starRatingThresholds[1]){
+      } else if (game.moves <= game.starRatingThresholds[1]) {
         starString = starActiveString + starActiveString + starInactiveString;
       } else {
         starString = starActiveString + starInactiveString + starInactiveString;
       };
       console.log($(this));
 
-      $(this).html("Rating: "+starString);
+      $(this).html(starString);
     });
+  },
+  checkCurrentPairOfCards: function(){
+    game.cardPairComparison = true;
+    if (game.gridOfCards[game.drawStepCards[0] - 1] !== game.gridOfCards[game.drawStepCards[1] - 1]) {
+      $('[data-card-position="' + game.drawStepCards[0] + '"], [data-card-position="' + game.drawStepCards[1] + '"]').addClass("memoryCard--mismatch");
+      var timeoutID = window.setTimeout(function() {
+          $('[data-card-position="' + game.drawStepCards[0] + '"], [data-card-position="' + game.drawStepCards[1] + '"]').removeClass("memoryCard--mismatch").css("background-image", game.defaultCardPicture);
+          game.cardPairComparison = false;
+        },
+        2000);
+    } else {
+      game.cardPairsFound += 1;
+      $('[data-card-position="' + game.drawStepCards[0] + '"], [data-card-position="' + game.drawStepCards[1] + '"]').addClass("found");
+      game.cardPairComparison = false;
+    }
+    game.moves += 1;
+    game.availableStepsPerDraw = 1;
+    game.updateGameStats();
+  },
+  checkIfAllPairsOfCardsFound: function(){
+    if (game.cardPairsFound === 8) {
+      game.endGame();
+      $("#wonModal .gameDuration").text("Duration: " + parseInt(game.gameDurationInSeconds / 60) + " minutes," + parseInt(game.gameDurationInSeconds % 60) + " seconds");
+      $("#wonModal .moves").text("Moves: " + parseInt(game.moves));
+      $("#wonModal .starRating").text(function() {
+        var starString = "";
+        var starActiveString = '<i class="fa fa-star" aria-hidden="true"></i>';
+        var starInactiveString = '<i class="fa fa-star-o" aria-hidden="true"></i>';
+        if (game.moves <= game.starRatingThresholds[2]) {
+          starString = starActiveString + starActiveString + starActiveString;
+        } else if (game.moves <= game.starRatingThresholds[1]) {
+          starString = starActiveString + starActiveString + starInactiveString;
+        } else {
+          starString = starActiveString + starInactiveString + starInactiveString;
+        };
+        console.log($(this));
+
+        $(this).html("Rating: " + starString);
+      });
+
+      window.setTimeout(function() {
+        // game.showDuration();
+        // game.showMoves();
+        // game.showStartRating(game.moves);
+        $('#wonModal').modal('show');
+      }, 1000);
+    }
   }
 }
 
@@ -103,14 +152,13 @@ game.initialize();
 
 // A function to execute when the DOM is fully loaded.
 $(function() {
-  $('[data-action="reset-game"]').click(function(){
+  $('[data-action="reset-game"]').click(function() {
     game.endGame();
     game.initialize();
     $('#wonModal').modal('hide');
-
   });
 
-  $('[data-card-position]:not(.found)').click(function(){
+  $('[data-card-position]:not(.found)').click(function() {
     // If two cards have been selected and compared, prevent other cards to be selected.
     if (game.cardPairComparison === true) {
       $(this).hide('fast').show('fast');
@@ -121,66 +169,21 @@ $(function() {
     game.startGame();
 
     var selectedCard = $(this);
-    var pictureUrlOfCard = game.picturesForCards[game.gridOfCards[selectedCard.data("cardPosition")-1]];
+    var pictureUrlOfCard = game.picturesForCards[game.gridOfCards[selectedCard.data("cardPosition") - 1]];
 
     selectedCard.css("background-image", pictureUrlOfCard);
-    game.drawStepCards[game.availableStepsPerDraw-1] = $(this).data("cardPosition");
-
-    // console.log("game.drawStepCards :"+game.drawStepCards);
-    // console.log("availableStepsPerDraw:"+game.availableStepsPerDraw);
+    game.drawStepCards[game.availableStepsPerDraw - 1] = $(this).data("cardPosition");
 
     switch (game.availableStepsPerDraw) {
       case 1:
         game.availableStepsPerDraw += 1;
         break;
       case 2:
-        game.cardPairComparison = true;
-        if (game.gridOfCards[game.drawStepCards[0]-1] !== game.gridOfCards[game.drawStepCards[1]-1]) {
-          $('[data-card-position="'+game.drawStepCards[0]+'"], [data-card-position="'+game.drawStepCards[1]+'"]').addClass("memoryCard--mismatch");
-          var timeoutID = window.setTimeout(function(){
-            $('[data-card-position="'+game.drawStepCards[0]+'"], [data-card-position="'+game.drawStepCards[1]+'"]').removeClass("memoryCard--mismatch").css("background-image", game.defaultCardPicture );
-            game.cardPairComparison = false;
-          },
-          2000);
-        } else {
-          game.cardPairsFound += 1;
-          $('[data-card-position="' + game.drawStepCards[0] + '"], [data-card-position="' + game.drawStepCards[1] + '"]').addClass("found");
-          game.cardPairComparison = false;
-        }
-        game.moves += 1;
-        game.availableStepsPerDraw = 1;
-        game.updateGameStats();
+        game.checkCurrentPairOfCards();
         break;
     }
 
-    if (game.cardPairsFound === 8) {
-      game.endGame();
-      $("#wonModal .gameDuration").text("Duration: " + parseInt(game.gameDurationInSeconds / 60) + " minutes," + parseInt(game.gameDurationInSeconds % 60) + " seconds");
-      $("#wonModal .moves").text("Moves: " + parseInt(game.moves));
-      $("#wonModal .starRating").text(function(){
-        var starString = "";
-        var starActiveString = '<i class="fa fa-star" aria-hidden="true"></i>';
-        var starInactiveString = '<i class="fa fa-star-o" aria-hidden="true"></i>';
-        if (game.moves == game.starRatingThresholds[2]) {
-          starString = starActiveString + starActiveString + starActiveString;
-        } else if (game.moves <= game.starRatingThresholds[1]){
-          starString = starActiveString + starActiveString + starInactiveString;
-        } else {
-          starString = starActiveString + starInactiveString + starInactiveString;
-        };
-        console.log($(this));
-
-        $(this).html("Rating: "+starString);
-      });
-
-      window.setTimeout(function () {
-        // game.showDuration();
-        // game.showMoves();
-        // game.showStartRating(game.moves);
-        $('#wonModal').modal('show');
-      },1000);
-    }
-
+    game.checkIfAllPairsOfCardsFound();
   });
 
 });
